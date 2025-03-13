ff7bd526f45d3ac9caa7f45e82b2f8321767a1a3
0sAf456 <ainaras448@gmail.com>
2025-03-13 15:56:47 +0200

    fixed the header a bit

--- Receptu Sandelys/src/components/Header.jsx
+++ Receptu Sandelys/src/components/Header.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
   return (
     <header className="bg-white text-black py-4 w-full">
       <div className="container mx-auto flex items-center justify-center">
-      <img src="/emptyFridge.png"
-        alt="Logo"
-        className="w-24 h-24 mr-4"
-        />
-      <h1 className="text-3xl font-bold">Recipe Finder</h1>
+        <img src="/emptyFridge.png" alt="Logo" className="w-24 h-24 mr-4" />
+        <h1 className="text-3xl font-bold">Receptu sandelys</h1>
       </div>
     </header>
   );
